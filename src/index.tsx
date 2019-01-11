c64a29e1909b48853824016f5492b3d830da3ea6
Jared M. Peterson <jared@peteyinc.com>
2019-01-10 20:05:31 -0700

    Initial commit from Create React App

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// relative imports
+import App from './components/App/App'
+
+// tslint:disable-next-line:no-import-side-effect
+import './index.scss'
+
+ReactDOM.render(<App />, document.getElementById('root'))
